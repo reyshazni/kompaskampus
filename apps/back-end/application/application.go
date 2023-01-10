3b496a2e3b989d18cf7bcf8db4ccdd203a840e0a
Farhandika <18220015@std.stei.itb.ac.id>
2023-01-10 12:35:13 +0700

    added middlewares

--- apps/back-end/application/application.go
+++ apps/back-end/application/application.go
@@ -4,30 +4,48 @@ import (
 	"FindMyDosen/config"
 	"FindMyDosen/model/entity"
 	"FindMyDosen/modules/auth"
+	"FindMyDosen/modules/university"
 	"github.com/golang-jwt/jwt/v4"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
+	"time"
 )
 
 func ApplicationDelegate() {
 	e := echo.New()
-	v1 := e.Group("/v1")
-	auth.AuthController(v1)
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	setupMiddlewares(e)
+	jwtMiddleware := getJWTMiddleware()
 	e.GET("/test", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
-	}, getJWTMiddleware())
+	}, jwtMiddleware)
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, echo.Map{
 			"msg": "ok",
 		})
 	})
+
+	v1 := e.Group("/v1")
+	auth.AuthController(v1)
+	university.UniController(v1, jwtMiddleware)
+
 	port := config.GetServerPort()
 	e.Logger.Fatal(e.Start(port))
+}
+
+func setupMiddlewares(e *echo.Echo) {
+	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		ErrorMessage: "Request Timeout!",
+		Timeout:      30 * time.Second,
+	}))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+	e.Use(middleware.Secure())
 }
 
 func getJWTMiddleware() echo.MiddlewareFunc {
--- apps/back-end/modules/university/university_controller.go
+++ apps/back-end/modules/university/university_controller.go
@@ -1,1 +1,12 @@
 package university
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func UniController(e *echo.Group, jwtMiddleware echo.MiddlewareFunc) {
+	e.GET("/university", func(c echo.Context) error {
+		return c.String(http.StatusOK, "uni")
+	}, jwtMiddleware)
+}
