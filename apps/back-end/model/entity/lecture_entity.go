96779275b1ed26490e8ff96f1039f2e8d40a3b59
Farhandika <18220015@std.stei.itb.ac.id>
2023-01-13 11:37:08 +0700

    add lecture rating components

--- apps/back-end/model/entity/lecture_entity.go
+++ apps/back-end/model/entity/lecture_entity.go
@@ -9,4 +9,8 @@ type Lecture struct {
 	University   University `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Faculty      string     `gorm:"type:varchar(100); NOT NULL"`
 	Href         string     `gorm:"type:varchar(100)"`
+	Recommended  float32    `gorm:"DEFAULT 0"`
+	Quality      float32    `gorm:"DEFAULT 0"`
+	Difficulty   float32    `gorm:"DEFAULT 0"`
+	Overall      float32    `gorm:"DEFAULT 0"`
 }
